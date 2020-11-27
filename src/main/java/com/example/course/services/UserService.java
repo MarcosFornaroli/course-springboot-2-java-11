8848d054d87189b4d93f2af42b371422eed1d3eb
MarcosFornaroligit config --global user.name MarcosFornaroligit config --global user.name MarcosFornaroli <marcosfornaroli@gmail.com>
2020-11-26 21:38:29 -0300

    User update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -11,14 +11,14 @@ import com.example.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
@@ -27,12 +27,26 @@ public class UserService {
 	public List<User> findByName(String name) {
 		return userRepository.findByName(name);
 	}
-	
-	public User insert (User obj) {
+
+	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+
+		updateDate(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateDate(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
 	}
 }
