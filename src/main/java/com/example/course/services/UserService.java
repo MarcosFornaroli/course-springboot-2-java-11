04258c084e69e013972cadd56e3bb2414b4229ee
MarcosFornaroligit config --global user.name MarcosFornaroligit config --global user.name MarcosFornaroli <marcosfornaroli@gmail.com>
2020-12-03 22:08:36 -0300

    Exception handling - update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.example.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -40,17 +42,22 @@ public class UserService {
 		try {
 			userRepository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException(id);			
+			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = userRepository.getOne(id);
+		try {
+			User entity = userRepository.getOne(id);
 
-		updateDate(entity, obj);
-		return userRepository.save(entity);
+			updateDate(entity, obj);
+			return userRepository.save(entity);			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateDate(User entity, User obj) {
